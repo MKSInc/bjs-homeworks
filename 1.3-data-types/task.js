08cdbf025f1ff04804bfc9c0a471950b25e7d962
Maksim <pyshkov.maksim@gmail.com>
2020-03-30 22:57:26 +0300

    1.3-data-types solution added

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,56 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    function getErrorText(paramName, paramValue) {
+        return `Параметр ${paramName} содержит неправильное значение ${paramValue}`;
+    }
+
+    if (isNaN(percent)) {
+        return getErrorText('percent', percent);
+
+    } else if (isNaN(contribution)) {
+        return getErrorText('contribution', contribution);
+
+    } else if (isNaN(amount)) {
+        return getErrorText('amount', amount);
+
+    } else {
+        const principal = amount - contribution;
+        percent = percent / 100 / 12;
+
+        function getCountOfMonths (mortgageDate) {
+            const currentDate = new Date();
+            const currentMonth = currentDate.getMonth() + 1;
+            const currentYear = currentDate.getFullYear();
+            const mortgageMonth = mortgageDate.getMonth() + 1;
+            const mortgageYear = mortgageDate.getFullYear();
+
+            if (mortgageYear - currentYear === 0) {
+                return mortgageMonth - currentMonth;
+
+            } else {
+                return 12 - currentMonth + mortgageMonth + 12 * (mortgageYear - currentYear - 1);
+            }
+        }
+
+        const countOfMonths = getCountOfMonths(date);
+        const payPerMonth = principal * (percent + percent / ((Math.pow((1 + percent), countOfMonths) - 1)));
+        let totalAmount = (payPerMonth * countOfMonths).toFixed(2);
+
+        return parseFloat(totalAmount);
+    }
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+
+    let userName = name;
+    if (name === '' || name === null || name === undefined) {
+        userName = 'Аноним';
+    }
+
+    console.log(`Привет, мир! Меня зовут ${userName}`);
+    return `Привет, мир! Меня зовут ${userName}`;
 }
